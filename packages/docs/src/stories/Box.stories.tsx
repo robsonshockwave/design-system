a685a660b10d74bdd3e0317a51696c32b7091cbc
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-06-12 23:25:39 -0300

    componente: Box/remoção componente Button

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import { Box, BoxProps } from '@robsonshockwave/react/src';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta;
+
+export const Primary: StoryObj<BoxProps> = {};
