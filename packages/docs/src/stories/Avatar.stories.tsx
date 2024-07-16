e2cb9970a378862cfdafc3a9a970e7eca86090e7
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-07-15 23:08:14 -0300

    storybook: componente de avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarProps } from '@robsonshockwave/react/src';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://avatars.dicebear.com/api/avataaars/john.svg',
+    alt: 'John',
+  },
+} as Meta<AvatarProps>;
+
+export const Primary: StoryObj<AvatarProps> = {};
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
