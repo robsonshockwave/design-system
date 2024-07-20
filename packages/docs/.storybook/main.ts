fe3ed3bb1e7463fdde7504eb762945350312a0de
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-07-20 11:11:11 -0300

    publicando o storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,12 @@ const config: StorybookConfig = {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/';
+    }
+
+    return config;
+  },
 };
 export default config;
